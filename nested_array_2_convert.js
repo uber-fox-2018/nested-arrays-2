3ae96db5adce31778866f87ffc29b70e7adad77b
sbachr <sbachr0501@gmail.com>
2018-07-11 19:37:07 +0700

    first commit nested array 2

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -2,25 +2,35 @@
 
 let roster = [
   ['Number', 'Name', 'Position', 'Points per Game'],
-  [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23] ],
-  [9, 'Ms. Buckets', 'Point Guard', [19, 0, 11, 22, 0] ],
-  [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25] ],
-  [7, 'Sally Talls', 'Power Forward ', [18, 29, 26, 31, 19] ],
-  [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0] ]
+  [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23]],
+  [9, 'Ms. Buckets', 'Point Guard', [19, 0, 11, 22, 0]],
+  [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25]],
+  [7, 'Sally Talls', 'Power Forward ', [18, 29, 26, 31, 19]],
+  [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0]]
 ]
 
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
-function convert_roster_format (nestedArray) {
+function convert_roster_format(nestedArray) {
   // your convert code here
-}
+  let result = []
+  for (let i = 1; i < nestedArray.length; i++) {
+    var temp = {}
+    for (let j = 0; j < nestedArray[i].length; j++) {
+      temp = { number: nestedArray[i][0], name: nestedArray[i][1], position: nestedArray[i][2], poinPerGame: nestedArray[i][3] }
 
+    }
+    result.push(temp)
+  }
+  return result
+}
 let object_roster = convert_roster_format(roster)
+console.log(object_roster[0])
 console.log(object_roster[2])
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
 
-console.log(object_roster[0]['Name'] == 'Joe Schmo') // outputs true
+console.log(object_roster[0]['name'] == 'Joe Schmo') // outputs true
 
 module.exports = {
   convert_roster_format
