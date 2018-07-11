02b197af7f39770baa64ba88847d2c8b6eee10a1
Grayfield <grayfieldmm@outlook.com>
2018-07-11 14:33:06 +0700

    Add some driver code for testing

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -8,8 +8,23 @@ let roster = [
   [7, 'Sally Talls', 'Power Forward ', [18, 29, 26, 31, 19]],
   [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0]]
 ]
-
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
+
+let students = [
+  ['Name', 'Address', 'Scores'],
+  ['Drake', 
+    {'Street': 'Grasshill 12', 'City': 'Greenwood', 'Region': 'Ashenvale'},
+    [{'Subject': 'Math', 'Score': '8'}, {'Subject': 'English', 'Score': '7'}, {'Subject': 'Biology', 'Score': '9'}] 
+  ],
+  ['Elizabeth', 
+    {'Street': 'Ruby 8', 'City': 'Redstone', 'Region': 'Jewelline'},
+    [{'Subject': 'Math', 'Score': '9'}, {'Subject': 'English', 'Score': '8'}, {'Subject': 'Biology', 'Score': '8'}] 
+  ],
+  ['Nero', 
+    {'Street': 'Blueflame 3', 'City': 'Fierydune', 'Region': 'Northren Helios'},
+    [{'Subject': 'Math', 'Score': '7'}, {'Subject': 'English', 'Score': '9'}, {'Subject': 'Biology', 'Score': '6'}] 
+  ],
+]
 
 function convert_roster_format(nestedArray) {
   var result = [];
@@ -30,12 +45,18 @@ function formatRoster(keyArr, valueArr) {
   return result;
 }
 
-let object_roster = convert_roster_format(roster)
-console.log(object_roster[2])
+//Driver code
+// let object_roster = convert_roster_format(roster)
+// console.log(object_roster[2])
 
 // => { "Number": 31, "Name": "Harvey Kay", "Position": "Shooting Guard", "Points per Game": [0, 30, 16, 0, 25] }
 
-console.log(object_roster[0]['Name'] == 'Joe Schmo') // outputs true
+//console.log(object_roster[0]['Name'] == 'Joe Schmo') // outputs true
+
+let object_student = convert_roster_format(students)
+console.log(object_student[0])
+console.log(object_student[1]['Address']['Region'])
+console.log(object_student[2]['Scores'][0]['Score'])
 
 module.exports = {
   convert_roster_format
