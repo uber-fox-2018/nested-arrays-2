043f8cccd649731e44b96dfdba750933ece56dd9
Grayfield <grayfieldmm@outlook.com>
2018-07-11 14:02:51 +0700

    Finished nested arrays 2 challenege

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -2,17 +2,32 @@
 
 let roster = [
   ['Number', 'Name', 'Position', 'Points per Game'],
-  [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23] ],
-  [9, 'Ms. Buckets', 'Point Guard', [19, 0, 11, 22, 0] ],
-  [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25] ],
-  [7, 'Sally Talls', 'Power Forward ', [18, 29, 26, 31, 19] ],
-  [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0] ]
+  [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23]],
+  [9, 'Ms. Buckets', 'Point Guard', [19, 0, 11, 22, 0]],
+  [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25]],
+  [7, 'Sally Talls', 'Power Forward ', [18, 29, 26, 31, 19]],
+  [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0]]
 ]
 
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
-function convert_roster_format (nestedArray) {
-  // your convert code here
+function convert_roster_format(nestedArray) {
+  var result = [];
+  let rosterObj = {};
+  for (let i = 1; i < nestedArray.length; i++) {
+    rosterObj = formatRoster(nestedArray[0], nestedArray[i]);
+    result.push(rosterObj);
+  }
+  return result;
+}
+
+function formatRoster(keyArr, valueArr) {
+  let result = {};
+  for(let i = 0; i < keyArr.length; i++) {
+    let attr = keyArr[i];
+    result[attr] = valueArr[i];
+  }
+  return result;
 }
 
 let object_roster = convert_roster_format(roster)
