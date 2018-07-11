250e3891149e41e190ce2d918b7eea03e2215758
gandryeanb <gstandrianb@gmail.com>
2018-07-11 14:07:07 +0700

    adding js file

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -12,7 +12,16 @@ let roster = [
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
 function convert_roster_format (nestedArray) {
-  // your convert code here
+  let res = []
+
+  for (let i = 1; i < nestedArray.length; i++) {
+    let subRes = {}
+    for (let j = 0; j < nestedArray[i].length; j++) {
+      subRes[nestedArray[0][j]] = nestedArray[i][j]
+    }
+    res.push(subRes)
+  }
+  return res
 }
 
 let object_roster = convert_roster_format(roster)
